454969267ae02a54b956e9b3d86c9347d5446c75
athuls8991@gmail.com <athuls8991@gmail.com>
2024-03-23 09:22:32 +0530

    add  DB_URL

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://athuls8991:2880@cluster0.ityc9mx.mongodb.net/cabDb').then(
+mongoose.connect(process.env.DB_URL).then(
     ()=>{
         console.log('connected');
     }
